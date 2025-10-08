b94373f0083d1f669324bd2e259dbea6cf3de2ac
therealtommy <sokolovai1887@ya.ru>
2025-10-08 21:06:34 +0700

    Add composable function CustomImage

---
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.modifierdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.modifierdemo.ui.theme.ModifierDemoTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ModifierDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun CustomImage(image: Int,  modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = null
+    )
+}
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    val mymodifier = modifier
+        .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
+
+    Text(
+        "Hello Compose",
+        mymodifier,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun  DefaultPreview() {
+    ModifierDemoTheme {
+        DemoScreen()
+    }
+}
